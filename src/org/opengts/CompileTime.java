cb17a952bdb7cb482b955a60d7ef2bef81ed8335
Lazar Ivanović <sreca_radost@yahoo.com>
2014-02-01 07:08:18 +0000

    Set execute permissions on bin/*.sh

--- src/org/opengts/CompileTime.java
+++ src/org/opengts/CompileTime.java
@@ -1,9 +1,9 @@
 package org.opengts;
 public class CompileTime
 {
-    // 2014/01/26 17:34:53 UTC
-    public static final long   COMPILE_TIMESTAMP    = 1390757693L;
-    public static final String COMPILE_DATETIME     = "2014/01/26 17:34:53 UTC";
+    // 2014/01/30 23:08:15 UTC
+    public static final long   COMPILE_TIMESTAMP    = 1391123295L;
+    public static final String COMPILE_DATETIME     = "2014/01/30 23:08:15 UTC";
     public static final String SERVICE_ACCOUNT_ID   = "opengts";
     public static final String SERVICE_ACCOUNT_NAME = "Open Source OpenGTS";
     public static final String SERVICE_ACCOUNT_KEY  = "";
